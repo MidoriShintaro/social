e1a6b9d0e7e9826295302b662592ff7b8013aee1
Raito <hatran12387@gmail.com>
2023-06-25 20:20:05 +0700

    initial commit

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const db_1 = __importDefault(require("./db/db"));
@@ -31,13 +32,11 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.static(path_1.default.join(__dirname, "/public")));
 app.use((0, helmet_1.default)());
-// app.use(
-//   cors({
-//     origin: urlClient,
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     credentials: true,
-//   })
-// );
+app.use((0, cors_1.default)({
+    origin: urlClient,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+}));
 app.use((0, cookie_parser_1.default)());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
